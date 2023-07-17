f7a565bface3eb0303f94007395fd7014af9af76
floretdemola <floretdemola@gmail.com>
2023-07-17 14:11:59 -0400

    added portfolio and resume

---
+++ my-app/src/components/ProjectPortfolio.js
@@ -0,0 +1,128 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const brewsomeWeatherApp = "https://floretdemola.github.io/brewsome-weather/";
+const brewsomeWeatherGitHub = "https://github.com/floretdemola/brewsome-weather";
+const techBlogApp = "https://geekblog2000.herokuapp.com/";
+const techBlogGitHub ="https://github.com/floretdemola/techblog";
+const noteTakerApp = "https://note-taker-floretdemola.herokuapp.com/";
+const noteTakerGitHub = "https://github.com/floretdemola/note-taker";
+const sciptScoutApp = "https://script-scout.herokuapp.com/";
+const sciptScoutGitHub = "https://github.com/TheMaddoxJ/ScriptScout"
+
+const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+const styles = {
+    card: {
+        background: "#aba9b3",
+        margin: 20,
+        borderRadius: 6,
+    },
+    heading: {
+        padding: "0 20px",
+        background: "#858585",
+        fontWeight: 600,
+        fontSize: 18,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        minHeight: 50,
+        lineHeeight: 3.5,
+    },
+    content: {
+        color: '#aba9b3',
+        textAlign:'justify',
+        padding: 20,
+        alightItems: "center",
+        display: "flex",
+    },
+    button: {
+        display: 'flex',
+        alignitems:'center',
+        color: '2c2c2c',
+    },
+    input: {
+        padding: 6,
+        height: 150,
+        width: "auto",
+    },
+    section: {
+        marginTop: 40,
+        marginBottom: 40,
+        backgroundColor: "#858585",
+        textalign: "center",
+        alignitems: "center",
+        justifycontent: "space-between",
+        padding: "0 20px",
+        fontSize: "large",
+    },
+};
+
+function Card() {
+    return (
+        <div style={styles.card}>
+            <h1 className="d-flex align-items-center" styles={styles.heading}>Portfolio</h1>
+
+            <div className="d-flex flex-wrap" style={styles.content}>
+                <section style={styles.section}>
+                    <h3>Brewsome Weather</h3>
+                    <input
+                        style={styles.input}
+                        type="image"
+                        src={require("../../../images/Project_Pictue_1.png")}
+                        alt="brewsomeweatherapp">
+                    </input>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(brewsomeWeatherApp)}>Brewsome Weather App</button>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(brewsomeWeatherGitHub)}>Brewsome Weather GitHub</button>
+                </section>
+                <br></br>
+                <section style={styles.section}>
+                    <h3>ScriptScout</h3>
+                    <input
+                        style={styles.input}
+                        type="image"
+                        src={require("../../../images/scriptscout.png")}
+                        alt="brewsomeweatherapp">
+                    </input>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(sciptScoutApp)}>ScriptScout App</button>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(sciptScoutGitHub)}>ScriptScout GitHub</button>
+                </section>
+                <br></br>
+                <section style={styles.section}>
+                    <h3>NoteTaker App</h3>
+                    <input
+                        style={styles.input}
+                        type="image"
+                        src={require("../../../images/noteTakeApp.png")}
+                        alt="brewsomeweatherapp">
+                    </input>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(noteTakerApp)}>NoteTaker App</button>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(noteTakerGitHub)}>NoteTaker GitHub</button>
+                </section>
+                <br></br>
+                <section style={styles.section}>
+                    <h3>Tech Blog</h3>
+                    <input
+                        style={styles.input}
+                        type="image"
+                        src={require("../../../images/techBlog.png")}
+                        alt="brewsomeweatherapp">
+                    </input>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(techBlogApp)}>Tech Blog App</button>
+                    <button style={styles.button}
+                    onClick={() => openInNewTab(techBlogGitHub)}>Tech Blog GitHub</button>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ my-app/src/components/ProjectResume.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const styles = {
+    card: {
+        background: "#aba9b3",
+        margin: 20,
+        borderRadius: 6,
+    },
+    heading: {
+        padding: "0 20px",
+        background: "#858585",
+        fontWeight: 600,
+        fontSize: 18,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        minHeight: 50,
+        lineHeeight: 3.5,
+    },
+    content: {
+        color: '#aba9b3',
+        textAlign:'justify',
+        padding: 20,
+        alightItems: "center",
+        display: "flex",
+    },
+    button: {
+        display: 'flex',
+        alignitems:'center',
+        color: '2c2c2c',
+    },
+    resume: {
+        display: "flex",
+        flexDirection: "column",
+        alignitems: "center",
+        width: "100vw",
+    }
+};
+
+function Card() {
+    return (
+        <div style={styles.card}>
+            <h1 style={styles.heading}>Resume</h1>
+            <div style={styles.content}>
+                <h2>Skills</h2>
+                <ul>
+                    <li>Project Management</li>
+                    <li>Agile Project Management</li>
+                </ul>
+                <h2>Languages</h2>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>SQL</li>
+                    <li>NoSQL</li>
+                </ul>
+                <h2>Applications</h2>
+                <ul>
+                    <li>Infor MRM</li>
+                    <li>Infor MRM Marketing Anayltics</li>
+                    <li>GitHub</li>
+                    <li>Heroku</li>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                </ul>
+                <h2>Tools</h2>
+                <ul>
+                    <li>VS Code</li>
+                    <li>Git</li>
+                    <li>Node</li>
+                    <li>Web APIs</li>
+                    <li>Third Parth APIs</li>
+                    <li>Server Side APIs</li>                    <li>VS Code</li>
+                    <li>OOP</li>
+                    <li>MVC</li>
+                    <li>PWA</li>
+                    <li>React</li>
+                    <li>MERN</li>
+                </ul>
+            </div>
+            <div style={styles.resume}>
+                <a href="https://docs.google.com/document/d/13wgubLoO05UT-smM2d87l9ZUgbWhbILTBTm4uI2aoXk/edit?usp=sharing" styles={styles.button}>Download Resume</a>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
